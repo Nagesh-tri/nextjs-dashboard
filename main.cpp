477a6eb574a77d0f9a568958c429535d7091e5c3
Nagesh Tripathi <satyam9mzp@gmail.com>
2024-04-22 15:09:01 +0530

    copied from vercel

---
+++ main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+
+	string str;
+    cin>>str;
+    string ans;
+    if (islower(str[0]) )
+    n f ./+c92   0      {
+
+        for(int i=0;i<str.size();i++){
+	    ans+=tolower(str[i]);
+        }
+        cout << ans;
+	}  
+    else{
+        for(int i=0;i<str.size();i++){
+	    ans+=toupper(str[i]);
+        }
+        cout << ans;
+    }
+    }
